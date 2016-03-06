6b7c402f806ccdb15ba7e495449ddb170c72dbce
em21 <emckenziee@ymail.com>
2016-03-06 15:01:23 +0000

    test

--- code/tango_with_django_project/rango/views.py
+++ code/tango_with_django_project/rango/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says: Hey there world!")
-
-# Create your views here.
+	
+	
+def about(request):
+	return HttpResponse("Rango says: this the about page")
