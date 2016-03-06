fdc1d18972e81f89d98224babcd717807d037579
em21 <emckenziee@ymail.com>
2016-03-06 22:09:55 +0000

    changes

--- rango/views.py
+++ rango/views.py
@@ -3,11 +3,13 @@ from django.http import HttpResponse
 
 def index(request):
 
-    context_dict = {'bold message': "I am bold font from the context"}
+    context_dict = {'boldmessage': "I am bold font from the context"}
     return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
-    return HttpResponse("Rango says: this is the about page")
+
+    context_dict ={'boldmessage': "I'm bold and prob no working"}
+    return render(request, 'rango/about.html', context_dict)
 
 # Create your views here.
